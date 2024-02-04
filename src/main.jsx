ba5ba0761f69beed0be8eaa8bfc7244c8924a373
Guilh3rme <guilhermereisefr@gmail.com>
2024-02-04 19:20:41 -0300

    backend-login

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './scss/main.scss'
-import { createMemoryRouter, MemoryRouter, Route, RouterProvider } from 'react-router-dom'
-import Routes from './pages/Routes.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./scss/main.scss";
+import { RouterProvider } from "react-router-dom";
+import Routes from "./pages/Routes.jsx";
 
-const router = Routes()
+const router = Routes();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,75 +1,96 @@
-import React from 'react'
-import './Login.scss'
-import foto from '../../assets/panorama-itaqui.jpg' 
-import logo from '../../assets/logo.png'
-import { useState } from 'react'
+import React from "react";
+import "./Login.scss";
+import foto from "../../assets/panorama-itaqui.jpg";
+import logo from "../../assets/logo.png";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    /* MAPEAR O CONTEÚDO DAS CAIXAS DE TEXTO */
-    const handleUsernameChange = event => {
-        setUsername(event.target.value);
-    };
-    const handlePasswordChange = event => {
-        setPassword(event.target.value);
-    };
-    
-    /* FUNÇÕES ATIVADAS AO CLICAR NO BOTÃO OU APERTAR ENTER */
-    const handleClick = event => {
-        if (username == 'admin' && password == 'admin') {
-            navigate("/atraqui");
-        }
-    };
-    const handleKeyDown = e => {
-        if (e.key === "Enter") {
-            handleClick();
-        }
-    };
+  /* MAPEAR O CONTEÚDO DAS CAIXAS DE TEXTO */
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
 
-    return (
-        <section id='login-around'>
-            <section id="login-container">
-                <section className="left">
-                    <div className="logo">ATRAQUI</div>
-                    <div className='inputs'>
-                        <i className="fa-solid fa-circle-user"></i>
-                        <input 
-                            type="text" 
-                            name="user" 
-                            id="user"
-                            placeholder="Usuário"
-                            value={username}
-                            onChange={handleUsernameChange}
-                            onKeyPress={handleKeyDown}
-                            autoComplete="off"
-                        />
-                        <input
-                            type="password" 
-                            name="password"
-                            id="password"
-                            placeholder="Senha"
-                            value={password}
-                            onChange={handlePasswordChange}
-                            onKeyPress={handleKeyDown}
-                            autoComplete="off"
-                        />
-                        <button className="login-button" onClick={handleClick}>Entrar</button>
-                    </div>
-                </section>
-                <section className="right">
-                    <div className='image-overlay'>
-                        <img src={logo}></img>
-                    </div>
-                    <div className='darker'></div>
-                    <img src={foto} alt="" />
-                </section>
-            </section>
+  /* Request Mapping for Login */
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({
+    login: username,
+    password: password,
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+  const handleClick = () => {
+    fetch("http://localhost:8080/auth/login", requestOptions)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Aconteceu um erro de rede!");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.token) {
+          navigate("/atraqui");
+        }
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  return (
+    <section id="login-around">
+      <section id="login-container">
+        <section className="left">
+          <div className="logo">ATRAQUI</div>
+          <div className="inputs">
+            <i className="fa-solid fa-circle-user"></i>
+            <input
+              type="text"
+              name="user"
+              id="user"
+              placeholder="Usuário"
+              value={username}
+              onChange={handleUsernameChange}
+              autoComplete="off"
+            />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Senha"
+              value={password}
+              onChange={handlePasswordChange}
+              autoComplete="off"
+            />
+            <button className="login-button" onClick={handleClick}>
+              Entrar
+            </button>
+          </div>
         </section>
-    )
-}
+        <section className="right">
+          <div className="image-overlay">
+            <img src={logo}></img>
+          </div>
+          <div className="darker"></div>
+          <img src={foto} alt="" />
+        </section>
+      </section>
+    </section>
+  );
+};
 
 export default Login;
